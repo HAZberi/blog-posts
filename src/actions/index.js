8448a3554209e68cf6cd6b8657a530362cff3a02
HAZberi <mhazuberi@gmail.com>
2021-01-13 14:54:08 -0700

    middleware connected

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
-export const getPosts = () => {
-    return {
-        type: "GET_POSTS_DATA"
-    }
-}
+import axios from "axios";
+import jasonPlaceholder from "../apis/jasonPlaceholder.js";
+import jsonplaceholder from "../apis/jasonPlaceholder.js";
+
+export const getPosts = () => async dispatch => {
+  const responseData = await jsonplaceholder.get("/posts");
+
+  dispatch({ type: "GET_POSTS_DATA", payload: responseData });
+};
