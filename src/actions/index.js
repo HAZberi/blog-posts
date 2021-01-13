6b516e1f0860618334bdc2cb1826787e4f526f00
HAZberi <mhazuberi@gmail.com>
2021-01-13 14:56:04 -0700

    fixed react warnings

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-import jasonPlaceholder from "../apis/jasonPlaceholder.js";
 import jsonplaceholder from "../apis/jasonPlaceholder.js";
 
 export const getPosts = () => async dispatch => {
