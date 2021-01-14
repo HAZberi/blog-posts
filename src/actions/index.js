8c86323cdcaf011cf81b6ec888f58647383a9eb7
HAZberi <mhazuberi@gmail.com>
2021-01-14 14:54:08 -0700

    Approach 3 refactor for deployment

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,7 +20,6 @@ export const getPosts = () => async (dispatch) => {
   dispatch({ type: "GET_USER_DATA", payload: responseData.data});
 } */
 
-
 //Approach 2
 /* export const getUser = id => dispatch => _getUser(id, dispatch);
 const _getUser = _.memoize(async (id, dispatch) => {
@@ -30,23 +29,22 @@ const _getUser = _.memoize(async (id, dispatch) => {
 
 //With this approach we are only able to fetch or get User one time
 //If in our application there is a need to re-fetch the same User our application
-//can break. For this particular example application - it works as expected. 
+//can break. For this particular example application - it works as expected.
 
 //Approach 3
-export const getUser = (id) => async dispatch => {
+export const getUser = (id) => async (dispatch) => {
   const responseData = await jsonplaceholder.get(`/users/${id}`);
-  dispatch({ type: "GET_USER_DATA", payload: responseData.data});
-}
+  dispatch({ type: "GET_USER_DATA", payload: responseData.data });
+};
 
-
-export const getPostsAndUsers = () => async (dispatch, getState) => {
+/* export const getPostsAndUsers = () => async (dispatch, getState) => {
   //We are manually dispatching actions as retruned functions via redux-thunk
   //Its important to call the action creator when passing one in dispatch.
   //thats how return function works
   await dispatch(getPosts());
   const userIds = _.uniq(_.map(getState().posts, "userId"));
   userIds.forEach(id => dispatch(getUser(id)));
-}
+} */
 
 //This approch is the best since we have now nested our action creators
 //we wait for to get the posts first then filter out unique user ids from posts
@@ -54,3 +52,12 @@ export const getPostsAndUsers = () => async (dispatch, getState) => {
 //array.
 
 //Following is a refactor that can be used in other applications
+
+export const getPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(getPosts());
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(getUser(id)))
+    .value();
+};
