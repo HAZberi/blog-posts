32fd4e1b9d17476b99f08c21d8ee9cafb52fd558
HAZberi <mhazuberi@gmail.com>
2021-01-14 09:47:02 -0700

    mapStateToProps for extracting logic

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,19 +2,18 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getUser } from "../actions";
 
-const UserHeader = ({ userId, users, getUser }) => {
+const UserHeader = ({ userId, user, getUser }) => {
 
     useEffect(()=>{
         getUser(userId);
     }, [getUser, userId])
 
-    const user = users.find(user => user.id === userId)
 
-  return <div>{user ? user.name : null}</div>;
+  return <div className="header">{user ? user.name : null}</div>;
 };
 
-const mapStateToProps = state => {
-    return { users : state.users }
+const mapStateToProps = (state, props) => {
+    return { user : state.users.find(user => user.id === props.userId) }
 }
 
 
