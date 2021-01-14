718552bd3eb2ea4ff82eb89f0a9d0bc8b24ac2ac
HAZberi <mhazuberi@gmail.com>
2021-01-14 13:41:30 -0700

    memoize action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,25 @@
 import jsonplaceholder from "../apis/jasonPlaceholder.js";
+import _ from "lodash";
 
-export const getPosts = () => async dispatch => {
+export const getPosts = () => async (dispatch) => {
   const responseData = await jsonplaceholder.get("/posts");
-  
+
   dispatch({ type: "GET_POSTS_DATA", payload: responseData.data });
 };
 
-export const getUser = (id) => async dispatch => {
+/* export const getUser = (id) => async dispatch => {
   const responseData = await jsonplaceholder.get(`/users/${id}`);
   dispatch({ type: "GET_USER_DATA", payload: responseData.data});
+} */
+
+export const getUser = function (id){
+  return function (dispatch) {
+    _getUser(id, dispatch);
+  }
 }
+
+
+const _getUser = _.memoize(async (id, dispatch) => {
+  const responseData = await jsonplaceholder.get(`/users/${id}`);
+  dispatch({ type: "GET_USER_DATA", payload: responseData.data });
+});
